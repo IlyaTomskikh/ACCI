a8fac209a2fdb8e8111eb8839b4a058ea812f977
Ilya Tomskikh <69895429+TeaEye@users.noreply.github.com>
2021-05-24 10:20:25 +0700

    Update Arithmetic.cpp

--- Arithmetic.cpp
+++ Arithmetic.cpp
@@ -2,10 +2,9 @@
 #include <map>
 #include <fstream>
 #include <algorithm>
-#include <string>
 #include <vector>
 #include <list>
-#include "Interval.hpp"
+#include "StringInterval.hpp"
 using namespace std;
 
 typedef unsigned char uchar;
@@ -25,10 +24,8 @@ map<uchar, double> alphabetReader(int *size)
             uchar _ch = orig.get();
             if (orig.eof()) break;
             tab[_ch]++;
-            //cout << _ch;
             ++counter;
         }
-        cout << endl;
     }
     orig.close();
     *size = counter;
@@ -42,7 +39,7 @@ list<Interval> freqsCreater(map<uchar, double> tab, int fileSize)
     for (map<uchar, double>::iterator iter = tab.begin(); iter != tab.end(); ++iter)
     {
         freqs[iter->first] = (iter->second) / fileSize;
-        cout << "freqs[" << iter->first << "] = " << freqs[iter->first] << endl;
+        //cout << "freqs[" << iter->first << "] = " << freqs[iter->first] << endl;
         intervals.push_back(Interval(tmp, tmp + iter->second / fileSize, iter->first));
         tmp += iter->second / fileSize;
     }
@@ -62,14 +59,41 @@ void mapSort(map<uchar, double> *tab)
     }
 }
 
+map<uchar, Interval> mapCreater(list<Interval> list_of_intervals)
+{
+    map<uchar, Interval> intervals;
+    for(list<Interval>::iterator iter = list_of_intervals.begin(); iter != list_of_intervals.end(); ++iter)
+    {
+        intervals[iter->getC()] = *iter;
+    }
+    return intervals;
+}
+
+double zip(map<uchar, Interval> intervals)
+{
+    ifstream input(original);
+    uchar _ch = input.get();
+    StringInterval res = intervals[_ch];
+    while(!input.eof())
+    {
+        _ch = input.get();
+        //if (input.eof()) break;
+        res += intervals[_ch];
+    }
+    input.close();
+    return res.getL();
+}
+
 int main()
 {
     int fileSize = 0;
     map<uchar, double> tab = alphabetReader(&fileSize);
-    cout << "fileSize = " << fileSize << endl;
-    list<Interval> intervals = freqsCreater(tab, fileSize);
-    for(list<Interval>::iterator iter = intervals.begin(); iter != intervals.end(); ++iter)
-        cout << "interval for symbol "<< iter->getC() << " [" << iter->getL() << "; " << iter->getH() << ')' << endl;
-    
+    //cout << "fileSize = " << fileSize << endl;
+    list<Interval> list_of_intervals = freqsCreater(tab, fileSize);
+    map<uchar, Interval> intervals = mapCreater(list_of_intervals);
+    for (map<uchar, Interval>::iterator iter = intervals.begin(); iter != intervals.end(); ++iter)
+    {
+        cout << iter->second;
+    }
     return 777;
 }
