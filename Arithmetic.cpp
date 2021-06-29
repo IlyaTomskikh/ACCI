f5f09fd50f6699594d04523327ddafd887c41cab
Ilya Tomskikh <69895429+TeaEye@users.noreply.github.com>
2021-06-29 18:29:18 +0700

    Update Arithmetic.cpp

--- Arithmetic.cpp
+++ Arithmetic.cpp
@@ -44,11 +44,10 @@ void zip(list<Interval> intervals, map<char, int> tab)
         compressed.write((char*)&iter->first, sizeof(iter->first));
         compressed.write((char*)&iter->second, sizeof(iter->second));
     }
-    ushort l, u = 65535;
+    ushort l = 0, u = 65535;
     int divisor = intervals.back().getU(),
     bitsToFollow = 0,
     counter = 0;
-    std::cout << "divisor is " << divisor << endl;
     ifstream orig(original, ios::binary);
     if (!orig.is_open()) exit(666);
     char toWrite = 0;
@@ -56,14 +55,11 @@ void zip(list<Interval> intervals, map<char, int> tab)
     {
         int current = orig.get();
         if (current == EOF) break;
-        std::cout << "current encoding symbol is " << current << endl;
         list<Interval>::iterator iter = intervals.begin();
         for (; iter != intervals.end() && iter->getC() != current; ++iter);
-        std::cout << "'iterator' After search:" << endl << *iter
-         << "current interval is [" << u << "; " << l << ')' << endl;
-        u = l + iter->getU() * (u - l + 1) / divisor - 1;
-        l += iter->getL() * (u - l + 1) / divisor;
-        std::cout << "new interval is [" << l << "; " << u << ')' << endl;
+        int range = u - l + 1;
+        u = l + iter->getU() * range / divisor - 1;
+        l += iter->getL() * range / divisor;
         while(1)
         {
             if (u < half)
@@ -126,10 +122,9 @@ void zip(list<Interval> intervals, map<char, int> tab)
     compressed.close();
 }
 
-void unzip(list<Interval> intervalsToUnzip, int fullLen)
+void unzip(list<Interval> intervalsToUnzip, int fullLen, ifstream &compressedF)
 {
-    ifstream compressedF(comp, ios::binary);
-    if (!compressedF.is_open()) exit(666);
+    
     ofstream decompressed(decomp, ios::binary);
     int divisor = intervalsToUnzip.back().getU(),
     val = (compressedF.get() << 8) | compressedF.get(),
@@ -141,8 +136,9 @@ void unzip(list<Interval> intervalsToUnzip, int fullLen)
         list<Interval>::iterator iter = intervalsToUnzip.begin();
         unsigned int freq = (((val - l + 1) * divisor) - 1) / (u - l + 1);
         for (; iter->getU() <= freq; ++iter);
-        u = l + iter->getU() * (u - l + 1) / divisor - 1;
-        l += iter->getL() * (u - l + 1) / divisor;
+        int range = u - l + 1;
+        u = l + iter->getU() * range / divisor - 1;
+        l += iter->getL() * range / divisor;
         while (1)
         {
             if (u < half)
@@ -182,78 +178,56 @@ void unzip(list<Interval> intervalsToUnzip, int fullLen)
 int main()
 {
     map<char, int> tab = tabCreater(original);
-    std::cout << "Alphabet size is " << tab.size() << endl;
-    for (map<char, int>::iterator iter = tab.begin(); iter != tab.end(); ++iter)
-        std::cout << iter->first << " - " << iter->second << endl;
     list<Interval> intervals;
     for (map<char, int>::iterator iter = tab.begin(); iter != tab.end(); ++iter)
     {
-        //lower, upper, number of symbols (key), symbol
         Interval tmpI(0, 0, iter->second, iter->first);
         intervals.push_back(tmpI);
     }
-    system("pause");
-    std::cout << "intervals before sort:" << endl;
-    for (list<Interval>::iterator iter = intervals.begin(); iter != intervals.end(); ++iter)
-        std::cout << *iter;
     intervals.sort(check());
     int tmpBound = 0;
-    std::cout << "intervals after sort:" << endl;
     for (list<Interval>::iterator iter = intervals.begin(); iter != intervals.end(); ++iter)
     {
         iter->setL(tmpBound);
         tmpBound += iter->getKey();
         iter->setU(tmpBound);
-        std::cout << *iter;
     }
-    system("pause");
     zip(intervals, tab);
     std::cout << "file '" << original << "' has been compressed and written in '" << comp << "' file" << endl;
     //compression ends here
-    system("pause");
 
     int fullLen = 0;
-    ifstream fin(comp, ios::binary);
+    ifstream compressedF(comp, ios::binary);
     map<char, int> freqs;
     int tabSize, key;
     char c;
-    fin.read((char*)&tabSize, sizeof(tabSize));
+    compressedF.read((char*)&tabSize, sizeof(tabSize));
     while(tabSize)
     {
-        fin.read((char*)&c, sizeof(c));
-        fin.read((char*)&key, sizeof(key));
+        compressedF.read((char*)&c, sizeof(c));
+        compressedF.read((char*)&key, sizeof(key));
         fullLen += key;
         freqs[c] = key;
         --tabSize;
     }
-    fin.close();
     list<Interval> intervalsToUnzip;
     for (map<char, int>::iterator iter = freqs.begin(); iter != freqs.end(); ++iter)
     {
         Interval tmpI(0, 0, iter->second, iter->first);
+        cout << tmpI;
         intervalsToUnzip.push_back(tmpI);
     }
-    std::cout << "intervalsToUnzip before sort:" << endl;
     for (auto iter = intervalsToUnzip.begin(); iter != intervalsToUnzip.end(); ++iter)
         std::cout << *iter;
     intervalsToUnzip.sort(check());
-    system("pause");
-    std::cout << "intervalsToUnzip after sort:" << endl;
-    for (auto iter = intervalsToUnzip.begin(); iter != intervalsToUnzip.end(); ++iter)
-        std::cout << *iter;
-    system("pause");
     tmpBound ^= tmpBound;
     for (auto iter = intervalsToUnzip.begin(); iter != intervalsToUnzip.end(); ++iter)
     {
         iter->setL(tmpBound);
-        std::cout << "l = " << tmpBound;
         tmpBound += iter->getKey();
         iter->setU(tmpBound);
-        std::cout << " u = " << tmpBound << ' ';
-        std::cout << *iter;
     }
-    system("pause");
-    unzip(intervalsToUnzip, fullLen);
+    unzip(intervalsToUnzip, fullLen, compressedF);
     std::cout << "file '" << comp << "' has been decompressed and written in '" << decomp << "' file" << endl;
     return 777;
 }
