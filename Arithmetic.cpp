f82ccbac83327e6ef2009f80d38bd0fbf2e2835e
Ilya Tomskikh <69895429+TeaEye@users.noreply.github.com>
2021-06-29 15:29:09 +0700

    Update Arithmetic.cpp

--- Arithmetic.cpp
+++ Arithmetic.cpp
@@ -8,188 +8,254 @@
 #include "Interval.hpp"
 using namespace std;
 
-typedef unsigned char uchar;
-typedef long double ldouble;
+typedef unsigned short ushort;
 
-map<uchar, ldouble> freqs;
 string comp = "compressedText.txt",
 decomp = "decompressedText.txt",
 original = "originalText.txt",
 frequency = "frequency.txt";
 
-int fs_to_dec;
+const ushort qtr1 = (65535 + 1) / 4, half = qtr1 * 2, qtr3 = qtr1 * 3;
 
-map<uchar, ldouble> alphabetReader(int *size)
+map<char, int> tabCreater(string txtFile)
 {
-    ifstream orig(original);
-    int counter = 0;
-    map<uchar, ldouble> tab;
-    if(orig.is_open())
+    ifstream fin(txtFile, ios::binary);
+    map<char, int> tab;
+    if (fin.is_open())
     {
-        while(true)
+        while(1)
         {
-            uchar _ch = orig.get();
-            if (orig.eof()) break;
-            tab[_ch]++;
-            ++counter;
+            unsigned char c = fin.get();
+            if (fin.eof()) break;
+            tab[c]++;
+        }
+    }
+    fin.close();
+    return tab;
+}
+
+
+
+void zip(list<Interval> intervals, map<char, int> tab)
+{
+    ofstream compressed(comp, ios::binary);
+    int tabSize = tab.size();
+    compressed.write((char*)&tabSize, sizeof(tabSize));
+    for (map<char, int>::iterator iter = tab.begin(); iter != tab.end(); ++iter)
+    {
+        compressed.write((char*)&iter->first, sizeof(iter->first));
+        compressed.write((char*)&iter->second, sizeof(iter->second));
+    }
+    ushort l, u = 65535;
+    int divisor = intervals.back().getU(),
+    bitsToFollow = 0,
+    counter = 0;
+    std::cout << "divisor is " << divisor << endl;
+    ifstream orig(original, ios::binary);
+    if (!orig.is_open()) exit(666);
+    char toWrite = 0;
+    while(!orig.eof())//swap 1
+    {
+        int current = orig.get();
+        if (current == EOF) break;
+        std::cout << "current encoding symbol is " << current << endl;
+        list<Interval>::iterator iter = intervals.begin();
+        for (; iter->getC() != current && iter != intervals.end(); ++iter);
+        std::cout << "'iterator' After search:" << endl << *iter
+         << "current interval is [" << u << "; " << l << ')' << endl;
+        u = l + iter->getU() * (u - l + 1) / divisor - 1;
+        l += iter->getL() * (u - l + 1) / divisor;
+        std::cout << "new interval is [" << u << "; " << l << ')' << endl;
+        while(1)
+        {
+            if (u < half)
+            {
+                ++counter;
+                if (counter == 8)
+                {
+                    compressed.put(toWrite);
+                    toWrite ^= toWrite;
+                    counter ^= counter;
+                }
+                for(; bitsToFollow > 0; --bitsToFollow)
+                {
+                    toWrite |= (1 << (7 - counter));
+                    ++counter;
+                    if (counter == 8)
+                    {
+                        compressed.put(toWrite);
+                        toWrite ^= toWrite;
+                        counter ^= counter;
+                    }
+                }
+            }
+            else if(l >= half)
+            {
+                toWrite |= (1 << (7 - counter));
+                ++counter;
+                if (counter == 8)
+                {
+                    compressed.put(toWrite);
+                    toWrite ^= toWrite;
+                    counter ^= counter;
+                }
+                for(; bitsToFollow > 0; --bitsToFollow)
+                {
+                    ++counter;
+                    if (counter == 8)
+                    {
+                        compressed.put(toWrite);
+                        toWrite ^= toWrite;
+                        counter ^= counter;
+                    }
+                }
+                u -= half;
+                l -= half;
+            }
+            else if (l >= qtr1 && u < qtr3)
+            {
+                ++bitsToFollow;
+                u -= qtr1;
+                l -= qtr1;
+            }
+            else break;
+            u <<= 1;
+            l <<= 1;
+            ++u;
         }
     }
     orig.close();
-    *size = counter;
-    return tab;
+    compressed.close();
 }
 
-list<Interval> freqsCreater(map<uchar, ldouble> tab, int fileSize)
+void unzip(list<Interval> intervalsToUnzip, int fullLen)
 {
-    list<Interval> intervals;
-    ldouble tmp = 0;
-    ofstream frequencyFile(frequency);
-    frequencyFile << tab.size();
-    for (map<uchar, ldouble>::iterator iter = tab.begin(); iter != tab.end(); ++iter)
-    {
-        freqs[iter->first] = (iter->second) / fileSize;
-        frequencyFile << iter->first << iter->second;
-        intervals.push_back(Interval(tmp, tmp + iter->second / fileSize, iter->first));
-        tmp += iter->second / fileSize;
-    }
-    frequencyFile << fileSize;
-    frequencyFile.close();
-    return intervals;
-}
-
-void mapSort(map<uchar, ldouble> *tab)
-{
-    list<ldouble> tmpToSort;
-    for (map<uchar, ldouble>::iterator iter = tab->begin(); iter != tab->end(); ++iter)
-        tmpToSort.push_back(iter->second);
-    tmpToSort.sort();
-    for (map<uchar, ldouble>::iterator iter = tab->begin(); iter != tab->end(); ++iter)
-    {
-        (*tab)[iter->first] = tmpToSort.back();
-        tmpToSort.pop_back();
-    }
-}
-
-map<uchar, Interval> mapCreater(list<Interval> list_of_intervals)
-{
-    map<uchar, Interval> intervals;
-    for(list<Interval>::iterator iter = list_of_intervals.begin(); iter != list_of_intervals.end(); ++iter)
-    {
-        intervals[iter->getC()] = *iter;
-    }
-    return intervals;
-}
-
-void zip(map<uchar, Interval> intervals)
-{
-    ifstream input(original);
-    ofstream output(comp), cons("cons.txt");
-    uchar _ch;
-    ldouble l = 0, u = 1, res = 0, tmpRes = 0;
-    string result = "";
-    while(1)
-    {
-        _ch = input.get();
-        if (input.eof()) break;
-        result.push_back(_ch);
-        
-        cons << "interval for '" << _ch <<
-        "' is [" << intervals[_ch].getL() <<
-        "; " << intervals[_ch].getU() << ')' <<
-        endl << "lower bound = " << intervals[_ch].getL() <<
-        ";\nupper bound = " << intervals[_ch].getU() << endl <<
-        "current l = " << l << ";\ncurrent u = " << u << endl;
-        
-        ldouble new_u = intervals[_ch].getU() * (u - l) + l;
-        ldouble new_l = intervals[_ch].getL() * (u - l) + l;
-        cons << "///recount///\nu = " << u << "; new_u = " << intervals[_ch].getU()
-        << " * (" << u << " - " << l << ") + " << l << " = " << new_u << endl
-        << "l = " << l << "; new_l = " << intervals[_ch].getL()
-        << " * (" << u << " - " << l << ") + " << l << " = " << new_l << endl;
-        if (new_u != new_l)
-        {
-            u = new_u;
-            l = new_l;
-            res = l / 2 + u / 2;    //problema v peres4ete granic (oni stanovyatsya ravny => res = 0 i zanovo)
-            
-        }
-        else
-        {
-            cons << "output res" << endl;
-            output << res;
-            //cout << "\nres = " << res << endl;
-            //printf("%Lg", res);
-            res = 0.0;
-        }
-        printf("%Lg\n", res);
-        //system("pause");
-        //cout << '{' << result << "} res = " << res << endl;
-        cons << "symbol '" << _ch << "' and current result is " << res << endl << endl;
-    }
-    input.close();
-    cons.close();
-    cons << "output res" << endl;
-    output << res;
-    output.close();
-}
-
-map<uchar, ldouble> freqsReader()
-{
-    map<uchar, ldouble> tab;
-    ifstream fr(frequency);
-    int size = fr.get();
-    while(size > 0)
-    {
-        uchar _ch = fr.get();
-        ldouble _ld = fr.get();
-        tab[_ch] = _ld;
-        --size;
-    }
-    fs_to_dec = fr.get();
-    fr.close();
-    for (map<uchar, ldouble>::iterator iter = tab.begin(); iter != tab.end(); ++iter) iter->second /= fs_to_dec;
-    return tab;
-}
-
-void unzip(map<uchar, Interval> intervals)
-{
-    ofstream output(decomp);
-    ldouble code = 0.0;
-    for (int i = 0; i < fs_to_dec - 1; ++i)
-    {
-        for (int j = 0; j < intervals.size() - 1; ++j)
-        {
-            if (code >= intervals[j].getL() && code >= intervals[j].getU())
-            {
-                output << intervals[j].getC();
-                code = (code - intervals[j].getL()) / (intervals[j].getU() - intervals[j].getL());
-                break;
-            }
-        }
-    }
+    
 }
 
 int main()
 {
-    int fileSize = 0;
-    map<uchar, ldouble> tab = alphabetReader(&fileSize);
-    //cout << "fileSize = " << fileSize << endl;
-    list<Interval> list_of_intervals = freqsCreater(tab, fileSize);
-    map<uchar, Interval> intervals = mapCreater(list_of_intervals);
-    for (map<uchar, Interval>::iterator iter = intervals.begin(); iter != intervals.end(); ++iter)
+    map<char, int> tab = tabCreater(original);
+    std::cout << "Alphabet size is " << tab.size() << endl;
+    for (map<char, int>::iterator iter = tab.begin(); iter != tab.end(); ++iter)
+        std::cout << iter->first << " - " << iter->second << endl;
+    list<Interval> intervals;
+    for (map<char, int>::iterator iter = tab.begin(); iter != tab.end(); ++iter)
     {
-        cout << iter->second;
+        //lower, upper, number of symbols (key), symbol
+        Interval tmpI(0, 0, iter->second, iter->first);
+        intervals.push_back(tmpI);
     }
-    cout << "zip" << endl;
-    zip(intervals);
-    tab = freqsReader();
-    list<Interval> listForDec = freqsCreater(tab, fs_to_dec);
-    map<uchar, Interval> intervals_to_dec = mapCreater(listForDec);
-    for (map<uchar, Interval>::iterator iter = intervals_to_dec.begin(); iter != intervals_to_dec.end(); ++iter)
+    system("pause");
+    std::cout << "intervals before sort:" << endl;
+    for (list<Interval>::iterator iter = intervals.begin(); iter != intervals.end(); ++iter)
+        std::cout << *iter;
+    intervals.sort(check());
+    int tmpBound = 0;
+    std::cout << "intervals after sort:" << endl;
+    for (list<Interval>::iterator iter = intervals.begin(); iter != intervals.end(); ++iter)
     {
-        cout << iter->second;
+        iter->setL(tmpBound);
+        tmpBound += iter->getKey();
+        iter->setU(tmpBound);
+        std::cout << *iter;
     }
-    cout << "unzip" << endl;
-    unzip(intervals_to_dec);
+    system("pause");
+    zip(intervals, tab);
+    std::cout << "file '" << original << "' has been compressed and written in '" << comp << "' file" << endl;
+    //compression ends here
+    system("pause");
+
+    int fullLen = 0;
+    ifstream fin(comp, ios::binary);
+    map<char, int> freqs;
+    int tabSize, key;
+    char c;
+    fin.read((char*)&tabSize, sizeof(tabSize));
+    while(tabSize)
+    {
+        fin.read((char*)&c, sizeof(c));
+        fin.read((char*)&key, sizeof(key));
+        fullLen += key;
+        freqs[c] = key;
+        --tabSize;
+    }
+    fin.close();
+    list<Interval> intervalsToUnzip;
+    for (map<char, int>::iterator iter = freqs.begin(); iter != freqs.end(); ++iter)
+    {
+        Interval tmpI(0, 0, iter->second, iter->first);
+        intervalsToUnzip.push_back(tmpI);
+    }
+    std::cout << "intervalsToUnzip before sort:" << endl;
+    for (auto iter = intervalsToUnzip.begin(); iter != intervalsToUnzip.end(); ++iter)
+        std::cout << *iter;
+    intervalsToUnzip.sort(check());
+    system("pause");
+    std::cout << "intervalsToUnzip after sort:" << endl;
+    for (auto iter = intervalsToUnzip.begin(); iter != intervalsToUnzip.end(); ++iter)
+        std::cout << *iter;
+    system("pause");
+    tmpBound ^= tmpBound;
+    for (auto iter = intervalsToUnzip.begin(); iter != intervalsToUnzip.end(); ++iter)
+    {
+        iter->setL(tmpBound);
+        std::cout << "l = " << tmpBound;
+        tmpBound += iter->getKey();
+        iter->setU(tmpBound);
+        std::cout << " u = " << tmpBound << ' ';
+        std::cout << *iter;
+    }
+    system("pause");
+    ifstream compressedF(comp, ios::binary);
+    if (!compressedF.is_open()) exit(666);
+    ofstream decompressed(decomp, ios::binary);
+    int divisor = intervalsToUnzip.back().getU(),
+    val = (compressedF.get() << 8) | compressedF.get(),
+    counter = 0;
+    char toDec = compressedF.get();
+    ushort l = 0, u = 65535;
+    while(fullLen)
+    {
+        list<Interval>::iterator iter = intervalsToUnzip.begin();
+        unsigned int freq = (((val - l + 1) * divisor) - 1) / (u - l + 1);
+        for (; iter->getU() <= freq; ++iter);
+        u = l + iter->getU() * (u - l + 1) / divisor - 1;
+        l += iter->getL() * (u - l + 1) / divisor;
+        while (1)
+        {
+            if (u < half)
+            {}
+            else if (l >= half)
+            {
+                val -= half;
+                l -= half;
+                u -= half;
+            }
+            else if (l >= qtr1 && u < qtr3)
+            {
+                val -= qtr1;
+                l -= qtr1;
+                u -= qtr1;
+            }
+            else break;
+            l <<= 1;
+            u <<= 1;
+            ++u;
+            val <<= 1;
+            val += (toDec & (1 << (7 - counter))) >> (7 - counter);
+            ++counter;
+            if (counter == 8)
+            {
+                counter ^= counter;
+                toDec = compressedF.get();
+            }
+        }
+        decompressed << iter->getC();
+        --fullLen;
+    }
+    compressedF.close();
+    decompressed.close();
+    std::cout << "file '" << comp << "' has been decompressed and written in '" << decomp << "' file" << endl;
     return 777;
 }
